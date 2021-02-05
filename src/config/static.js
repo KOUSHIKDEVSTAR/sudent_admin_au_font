7a753c513062494eed18068a381e8b947f07fc7c
Koushik Pramanick <46644710+KOUSHIKDEVSTAR@users.noreply.github.com>
2021-02-06 01:40:58 +0530

    TEST mysql 2

--- src/config/static.js
+++ src/config/static.js
@@ -1,21 +1,22 @@
-export const BASE_URL = () => {
+// export const BASE_URL = siteUrl(){
     
 
-    if(process.env.NODE_ENV === 'development'){
-        console.log('development', 'http://localhost:9000');
-        return 'http://localhost:9000';
-      }else if(process.env.NODE_ENV === 'production'){
-        console.log('production', 'https://jungle99.herokuapp.com');
-        return "http://3.25.66.75:9000/";
-      }else if(process.env.NODE_ENV === 'test'){
-        console.log('test');
-        return 'http://localhost:9000';
-      }else{
-        console.log('else', 'http://localhost:9000');
-        return 'http://localhost:9000';
-      }
-};
+//     if(process.env.NODE_ENV === 'development'){
+//         console.log('development', 'http://localhost:9000');
+//         return 'http://localhost:9000';
+//       }else if(process.env.NODE_ENV === 'production'){
+//         console.log('production', 'https://jungle99.herokuapp.com');
+//         return "http://3.25.66.75:9000/";
+//       }else if(process.env.NODE_ENV === 'test'){
+//         console.log('test');
+//         return 'http://localhost:9000';
+//       }else{
+//         console.log('else', 'http://localhost:5000');
+//         return 'http://localhost:9000';
+//       }
+// };
+// export const SITE_NAME = "Nazox";
+export const BASE_URL = "https://fast-stream-39179.herokuapp.com/";
 export const SITE_NAME = "Nazox";
-
 
 
